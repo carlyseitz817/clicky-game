ef404a922090f8bf1df618cdd8316f6f5b9d26a8
Carly Seitz <carlyseitz817@gmail.com>
2019-05-06 18:04:58 -0500

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="copy">
+            <p>&copy; Copyright 2019, Carly Seitz</p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
